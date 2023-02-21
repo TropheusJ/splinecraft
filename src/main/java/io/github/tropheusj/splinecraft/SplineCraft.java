a6beb6b64ab2fab531a949efbe0dee14263b6019
TropheusJ <jverrellijr@icloud.com>
2023-02-20 23:16:44 -0500

    finish setup

--- src/main/java/io/github/tropheusj/splinecraft/SplineCraft.java
+++ src/main/java/io/github/tropheusj/splinecraft/SplineCraft.java
@@ -1,4 +1,4 @@
-package io.github.tropheusj.modid_test;
+package io.github.tropheusj.splinecraft;
 
 import net.fabricmc.api.ModInitializer;
 
@@ -7,8 +7,8 @@ import net.minecraft.resources.ResourceLocation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleModTest implements ModInitializer {
-	public static final String ID = "modid_test";
+public class SplineCraft implements ModInitializer {
+	public static final String ID = "splinecraft";
 	public static final Logger LOGGER = LoggerFactory.getLogger(ID);
 
 	@Override
